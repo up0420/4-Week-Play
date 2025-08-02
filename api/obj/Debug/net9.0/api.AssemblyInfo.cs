ed9eab2adc91430136147977563d651c3a8c9a7d
donghyun <up0420@naver.com>
2025-08-02 11:10:17 +0900

    feat(api): MariaDB + CRUD + Swagger + Hangfire + SignalR skeleton2

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c7d15a8b70d03266b31843776f024b304c0934e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2cb64454a678f7e8816e5dfe90ce2dbbd6416c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
